d736471b71000c3c4945c90040a58e4bd7a7d1e7
taverasmisael <macrot29@gmail.com>
2018-02-10 23:12:55 -0400

    feat(ConditionList): complete and integrate with Condition component

--- src/components/Condition/Condition.js
+++ src/components/Condition/Condition.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import Typography from 'material-ui/Typography'
 import FormControlLabel from 'material-ui/Form/FormControlLabel'
-import Switch from 'material-ui/Switch'
+import Checkbox from 'material-ui/Checkbox'
 import Grid from 'material-ui/Grid'
 import IconButton from 'material-ui/IconButton'
 import DeleteIcon from 'material-ui-icons/Delete'
@@ -12,36 +12,37 @@ import MultiSelect from '../MultiSelect'
 
 class Condition extends PureComponent {
   static propTypes = {
+    onDelete: PropTypes.func.isRequired,
     mode: PropTypes.bool,
     conditionName: PropTypes.string.isRequired,
-    activeConditions: PropTypes.arrayOf(PropTypes.string),
+    value: PropTypes.arrayOf(PropTypes.string),
     conditionsList: PropTypes.object
   }
 
   static defaultProps = {
     mode: false,
-    activeConditions: [],
+    value: [],
     conditionsList: []
   }
   state = {
     switchText: 'Is'
   }
   onModeChange = (event, mode) => this.setState({ mode, switchText: mode ? 'Is' : 'Is not' })
-  onChangeCondition = ({ target }) => this.setState({ activeConditions: target.value })
+  onChangeCondition = ({ target }) => this.setState({ value: target.value })
 
   componentWillMount() {
-    const { mode, conditionName, conditionsList, activeConditions } = this.props
-    this.setState({ mode, conditionName, conditionsList, activeConditions, switchText: mode ? 'Is' : 'Is not' })
+    const { mode, conditionName, conditionsList, value } = this.props
+    this.setState({ mode, conditionName, conditionsList, value, switchText: mode ? 'Is' : 'Is not' })
   }
 
   render() {
-    const { mode, conditionName, switchText, activeConditions, conditionsList } = this.state
-    const { onDelete, id } = this.props
+    const { mode, conditionName, switchText, value, conditionsList } = this.state
+    const { onDelete } = this.props
     return (
       <Grid container spacing={16} alignItems="flex-end">
         <Grid item xs={12} sm={6} md={3}>
           <Typography variant="title">{conditionName}</Typography>
-          <FormControlLabel control={<Switch checked={mode} onChange={this.onModeChange} />} label={switchText} />
+          <FormControlLabel control={<Checkbox checked={mode} onChange={this.onModeChange} />} label={switchText} />
         </Grid>
         <Grid item xs={10} sm={4} md={7}>
           <MultiSelect
@@ -49,12 +50,12 @@ class Condition extends PureComponent {
             label="Conditions"
             placeholder="Select the conditions"
             onChange={this.onChangeCondition}
-            value={activeConditions}
+            value={value}
             list={conditionsList}
           />
         </Grid>
         <Grid item xs={2}>
-          <IconButton aria-label="Delete" onClick={onDelete(id)}>
+          <IconButton aria-label="Delete" onClick={onDelete}>
             <DeleteIcon />
           </IconButton>
         </Grid>
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,38 +1,41 @@
 import React, { PureComponent, Fragment } from 'react'
 
-import Condition from '../../components/Condition'
+import ConditionsList from '../../lists/ConditionsList'
 class App extends PureComponent {
+  state = {
+    conditions: {
+      devices: {
+        10: { id: '10', value: 'Android' },
+        20: { id: '20', value: 'iPhone' },
+        30: { id: '30', value: 'BB' },
+        40: { id: '40', value: 'Nokia' }
+      },
+      countries: {
+        1: { id: '1', value: 'Barbados' },
+        2: { id: '2', value: 'Bermudas' },
+        3: { id: '3', value: 'Know this' },
+        4: { id: '4', value: 'Angel' },
+        5: { id: '5', value: 'Emotion' }
+      }
+    },
+    activeConditions: [
+      { id: 0, conditionName: 'Device', conditionKey: 'devices', mode: true, value: ['20', '30'] },
+      { id: 1, conditionName: 'Countries', conditionKey: 'countries', mode: false, value: [] }
+    ]
+  }
+
+  onDeleteCondition = id => {
+    const activeConditions = this.state.activeConditions.filter(c => c.id !== id)
+    this.setState({ activeConditions })
+  }
   render() {
     return (
       <Fragment>
-        <Condition
-          id={0}
-          conditionName="Operating System"
-          mode={false}
-          conditionsList={{
-            0: {
-              id: '0',
-              value: 'Android'
-            },
-            1: {
-              id: '1',
-              value: 'iOS'
-            },
-            2: {
-              id: '2',
-              value: 'BB'
-            },
-            3: {
-              id: '3',
-              value: 'Window'
-            },
-            4: {
-              id: '4',
-              value: 'Mac'
-            }
-          }}
-          onDelete={id => () => console.log(id)}
-        />
+        <ConditionsList
+          onDelete={this.onDeleteCondition}
+          conditions={this.state.conditions}
+          activeConditions={this.state.activeConditions}
+        />{' '}
       </Fragment>
     )
   }
