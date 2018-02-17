91b4bd8d7d6359ff3daeeb1c2c2d6c12f6fd2972
taverasmisael <macrot29@gmail.com>
2018-02-16 23:14:34 -0400

    fix(uniqueKey): ensuere unique keys in SelectItems

--- src/components/MultiSelect/SelectItems.jsx
+++ src/components/MultiSelect/SelectItems.jsx
@@ -6,10 +6,10 @@ import MenuItem from 'material-ui/Menu/MenuItem'
 import ListItemText from 'material-ui/List/ListItemText'
 
 const SelectItems = ({ items = {}, value, ...props }) =>
-  Object.keys(items).map(k => {
+  Object.keys(items).map((k, i)=> {
     const item = items[k]
     return (
-      <MenuItem key={item.id} value={item.id} {...props}>
+      <MenuItem key={i} value={item.id} {...props}>
         <Checkbox checked={value.includes(item.id)} />
         <ListItemText primary={item.text} />
       </MenuItem>
@@ -20,7 +20,7 @@ SelectItems.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      value: PropTypes.string.isRequired
+      text: PropTypes.string.isRequired
     })
   ),
   value: PropTypes.arrayOf(PropTypes.string)
