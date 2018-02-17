b92786e5329db3002c93a4fb2cfa8ebac9cc69c5
taverasmisael <macrot29@gmail.com>
2018-02-16 23:48:41 -0400

    refact(initdata): use Normalizers instead of mappers

--- src/services/initdata.js
+++ src/services/initdata.js
@@ -1,19 +1,21 @@
 import { InitData } from './api'
 
-import { mapConditions, mapCampaign } from './mappers'
+import { NormalizeConditions } from './normalizers'
 
 export const GetInitialState = async id => {
-  const EmptyCampaign = { name: '', vertical: '', subVertical: '' }
+  const EmptyCampaign = { campaign: { name: '', vertical: '', subVertical: '' }, defaultOffers: [], rules: [] }
   try {
     const initData = await InitData(id)
-    const { verticals, connections, countries, devicesTypes: deviceTypes, campaign: campaignData } = initData
-    const campaign = Array.isArray(campaignData) ? EmptyCampaign : mapCampaign(campaignData)
-    const initialState = {
-      verticals,
-      campaign,
-      conditions: mapConditions({ connections, countries, deviceTypes })
-    }
-    return initialState
+    const { verticals, connections, countries, devicesTypes, campaign: campaignData } = initData
+    const conditions = NormalizeConditions({ connections, countries, devicesTypes }).entities
+    const { campaign, defaultOffers, rules } = Array.isArray(campaignData)
+      ? EmptyCampaign
+      : {
+          name: campaignData.name,
+          vertical: campaignData.vertical_id,
+          subVertical: campaignData.subvertical_id
+        }
+    return { campaign, defaultOffers, rules, verticals, conditions }
   } catch (error) {
     console.error(error)
   }
