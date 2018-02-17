f299fd186daf4651539e1109d2e55d51c8c6b476
taverasmisael <macrot29@gmail.com>
2018-02-16 23:47:42 -0400

    feat(api): use different URLs edit and create
    Remove mapper service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import fetch from 'unfetch'
 
+const EDITURL =
+  'https://gist.githubusercontent.com/anonymous/a84ddc3edb4e6baf7c0ad0d72c9b2a4e/raw/f268371916e48dccc7ed645164ac559695399d98/json'
+const CREATEURL =
+  'https://gist.githubusercontent.com/anonymous/b9dbf5e6956afe6302c55ebf5c9a3446/raw/a9cd567109ec82edc5ad4be2404fe6c54df53e9f/json'
 export const InitData = async id => {
-  const BaseUrl = 'https://gist.githubusercontent.com/anonymous/b9dbf5e6956afe6302c55ebf5c9a3446/raw/a9cd567109ec82edc5ad4be2404fe6c54df53e9f/json'
+  const BaseUrl = id ? EDITURL : CREATEURL
   try {
     const res = await fetch(`${BaseUrl}${id ? `?id=${id}` : ''}`)
     if (res.ok) {
