e1330c0ed56325d4b988fadf03da4a715adbb999
taverasmisael <macrot29@gmail.com>
2018-02-19 22:42:26 -0400

    feat(refact): move mapOffers to normalizer and transform to point-free

--- src/services/models/rule.js
+++ src/services/models/rule.js
@@ -1,25 +1,9 @@
-import { map } from 'ramda'
-import { conditionsLabels } from './conditions'
-
-const mapOffer = offer => ({ value: offer.id, ...offer })
-
-const mapCondition = ({ id, condition_id, values, comparation_expression }) => {
-  const { label: conditionName, value: conditionKey } = conditionsLabels[condition_id]
-  const mode = comparation_expression === '>'
-  const value = values.join(',')
-  return {
-    id,
-    mode,
-    value,
-    conditionName,
-    conditionKey
-  }
-}
+import { mapConditions, mapOffers } from '../normalizers'
 export default class Rule {
   constructor({ id = '', campaign_link_id: campaign = '', conditions = [], offers = [] }) {
     this.id = id
     this.campaign = campaign
-    this.activeConditions = map(mapCondition, conditions)
-    this.activeOffers = map(mapOffer, offers)
+    this.activeConditions = mapConditions(conditions)
+    this.activeOffers = mapOffers(offers)
   }
 }
--- src/services/normalizers.js
+++ src/services/normalizers.js
@@ -2,6 +2,7 @@ import { normalize as normalizr } from 'normalizr'
 import { flip, curry, map, reduce } from 'ramda'
 
 import ConditionsSchema from './schemas/conditions'
+import { conditionsLabels } from './models/conditions'
 
 const normalize = curry(flip(normalizr))
 
@@ -17,5 +18,17 @@ export const getRealtionKeys = (key, primary, foregin) =>
   unique(reduce((acc, curr) => [...acc, ...foregin[curr][key]], [], primary))
 
 export const mapKeysToValues = (keys, values) => map(k => values[k], keys)
+export const mapOffers = map(offer => ({ value: offer.id || offer.offer_id, id: offer.id || offer.offer_id, ...offer }))
+export const mapConditions = map(({ id, condition_id, values, comparation_expression }) => {
+  const { label: conditionName, value: conditionKey } = conditionsLabels[condition_id]
+  const mode = comparation_expression === '>'
+  const value = values.join(',')
+  return {
+    id,
+    mode,
+    value,
+    conditionName,
+    conditionKey
+  }
+})
 export const NormalizeConditions = normalize(ConditionsSchema)
-export const NormalizeCampaign = normalize()
